ed896af3898a3b68c4be63da6b4cd60cc2b8e17f
SimonLosson <72396583+SimonLosson@users.noreply.github.com>
2026-01-16 02:31:29 +0100

    feat!(card_deck_manager): adding abstraction, creating new methods, fixing Card (#8)
    * fix(card): fix comparison and numeric value
    
    * feat!(dealer): give_card method can handle any stack
    
    * feat!(player): adding stacks to player and editingdraw_card and play_card methods
    
    * feat(stack): adding add_cards and empty methods
    
    * chore(card_deck_manager): updating version

--- src/card_deck_manager/player.py
+++ src/card_deck_manager/player.py
@@ -6,10 +6,13 @@ from card_deck_manager.stack import TOP, Stack
 class Player:
     """Represents a player."""
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, stack_names: list[str] | None = None) -> None:
         """Init Player object."""
+        if stack_names is None:
+            stack_names = []
         self.name = name
         self.hand: Stack = Stack(name=f"{self.name}'s hand")
+        self.stacks = {stack_name : Stack(stack_name) for stack_name in stack_names}
 
     def __repr__(self) -> str:
         """Representation of the player object."""
@@ -19,25 +22,34 @@ class Player:
         """Str cast of the player object."""
         return f"The player's name is {self.name}"
 
-    def draw_card(self, target_stack: Stack, n: int = 1, add_position: str = TOP) -> None:
+    def draw_card(
+            self,
+            origin_stack: Stack,
+            dest_stack: Stack | None = None,
+            n: int = 1,
+            origin_position: str = TOP,
+            dest_position: str = TOP) -> None:
         """
         Draw n cards in a selected stack.
 
-        target_stack(Stack): stack in which the player draws,
+        origin_stack(Stack): stack in which the player draws,
+        dest_stack(Stack): stack in which the card is added (default is self.hand),
         n(int): number of cards to draw,
-        add_position(str): position to place the card in the player's stack.
+        dest_position(str): position to place the card in the dest stack.
         """
+        if dest_stack is None:
+            dest_stack = self.hand
         for _ in range(n):
             try:
-                card = target_stack.pick_card()
+                card = origin_stack.pick_card(position=origin_position)
             except AttributeError as e:
                 raise NotAStackError from e
-            self.hand.add_card(new_card=card, position=add_position)
+            dest_stack.add_card(new_card=card, position=dest_position)
 
-    def play_card(self, target_stack: Stack, n: int = 1, play_position: str = TOP) -> None:
-        """Play n cards from the player's hand to the target_stack."""
-        if not isinstance(target_stack, Stack):
+    def play_card(self, dest_stack: Stack, n: int = 1, play_position: str = TOP) -> None:
+        """Play n cards from the player's hand to the dest_stack."""
+        if not isinstance(dest_stack, Stack):
             raise NotAStackError
         for _ in range(n):
             card = self.hand.chose_card()
-            target_stack.add_card(new_card=card, position=play_position)
+            dest_stack.add_card(new_card=card, position=play_position)
